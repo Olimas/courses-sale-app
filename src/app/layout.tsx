13a58b23b868b8a1f8aeff38012bfeec02641fbc
Ivan <ivan.olimas@gmail.com>
2024-01-24 14:45:01 +0300

    chor: add prettier and shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css'
+
+import { cn } from '@/shared/ui/utils'
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,11 +18,16 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>
+      {children}
+      </body>
     </html>
-  );
+  )
 }
