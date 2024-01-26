3b60ae60fb99bae6752f255ecdea3327ef6fa46a
Ivan <ivan.olimas@gmail.com>
2024-01-26 18:34:35 +0300

    feat: add base auth with email

--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -1,5 +1,6 @@
 import { AuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import EmailProvider from 'next-auth/providers/email'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { compact } from 'lodash-es'
 
@@ -9,6 +10,18 @@ import { privateConfig } from '@/shared/config/private'
 export const nextAuthConfig: AuthOptions = {
   adapter: PrismaAdapter(dbClient) as AuthOptions['adapter'],
   providers: compact([
+    EmailProvider({
+      server: {
+        host: privateConfig.EMAIL_SERVER_HOST,
+        port: privateConfig.EMAIL_SERVER_PORT,
+        auth: {
+          user: privateConfig.EMAIL_SERVER_USER,
+          pass: privateConfig.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: privateConfig.EMAIL_FROM,
+    }),
+
     privateConfig.GITHUB_ID &&
       privateConfig.GITHUB_SECRET &&
       GithubProvider({
